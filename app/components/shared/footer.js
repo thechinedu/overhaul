ac2957f43e63f20e086ffeb9a62d6a2cba497860
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-06-27 16:24:30 +0100

    Implement sticky footer

--- app/components/shared/footer.js
+++ app/components/shared/footer.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render () {
     return (
       <footer className="footer">
-        Made with love by not Seun Osewa
+        Made with <i className="fa fa-heart"></i> by not Seun Osewa
       </footer>
     );
   }
