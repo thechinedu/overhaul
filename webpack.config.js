e8946507a4dac0055ddab31312635ca264583500
Chinedu Daniel <chinedudaniel7@gmail.com>
2018-07-10 09:32:45 +0100

    Add configuration for production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 var path = require('path');
+var webpack = require('webpack');
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: './app/index.js',
+var config = {
+  entry: ['babel-regenerator-runtime', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
   module: {
     rules: [
-      { test: /\.js$/, use: 'babel-loader' },
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
       { test: /\.scss$/, use: [ 'style-loader', 'css-loader', 'sass-loader' ] }
     ],
   },
@@ -20,3 +22,23 @@ module.exports = {
     }
   }
 }
+
+if (process.env.NODE_ENV === 'production') {
+  Object.assign(config, {
+    devtool: 'none',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }
+      }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          compress: true
+        }
+      })
+    ]
+  });
+}
+
+module.exports = config;
